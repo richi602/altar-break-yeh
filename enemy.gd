112ed5746e9ac2b9ddaf1b0552b6b834a52b04dd
noisemaker209 <noisemaker209@gmail.com>
2026-07-27 23:00:40 -0700

    Adds simple aggro system
    - Getting close to enemy make it face the player
    
    - Shooting enemy also makes it face you

--- enemy.gd
+++ enemy.gd
@@ -4,11 +4,27 @@ extends CharacterBody3D
 @export var max_health : float = 100
 var health : float
 
-
+@export var aggroRadius : Area3D
+var Player : CharacterBody3D
+var aggro := false
 
 func _ready() -> void:
 	health = max_health
+	#print(aggro)
 	
+func _physics_process(delta: float) -> void:
+	if Player != null:
+		var lastRotation = rotation
+		look_at(Player.position,Vector3.UP, true)
+		rotation = Vector3(0, rotation.y, 0)
+	
+
+func _on_aggro_radius_body_entered(body: Node3D) -> void:
+	
+	if body.name == "Player":
+		Player = body
+		aggro = true
+		print(aggro)
 	
 func get_all_meshes(node):
 
@@ -102,8 +118,8 @@ func take_damage(amount):
 
 	health -= amount
 	print("Health:", health)
-
 	flash_red()
+	aggroRadius.scale *= 4
 
 	if health <= 0:
 		queue_free()
